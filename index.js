ca7491ff51a3d3cfb6b9bbc62e00219ad922404d
SayujK <105600499+SayujK@users.noreply.github.com>
2022-09-01 20:47:00 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+function discord(){
+    location="https://discord.com/invite/Zmc6ZSuHAV"
+}
+function donate(){
+    location="https://app.streamersalert.com/donate/sayuj"
+}
+function facebook(){
+    location="https://facebook.com/sayujyt"
+}
+function insta(){
+    location="https://instagram.com/sayujyt"
+}
+function tiktok(){
+    location="https://tiktok.com/@sayujyt"
+}
+function youtube(){
+    location="https://www.youtube.com/channel/UCrFufyA0w36YdD-1zmw-ZLg?sub_confirmation=1"
+}
+function twitter(){
+    location="https://twitter.com/sayujyt"
+}
