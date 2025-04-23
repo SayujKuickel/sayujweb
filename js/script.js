6415afadb1ba829c1a086fb3856e4e67e1347ad1
sayujkuickel <sayujkuickel@gmail.com>
2025-04-23 13:10:07 +0545

    add

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const backToTopBtn = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    backToTopBtn.style.display = "block";
+  } else {
+    backToTopBtn.style.display = "none";
+  }
+});
+
+document.getElementById("backToTop").addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
